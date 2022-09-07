d98e0615b62dade771d0f915ef8e84d5c2faf3de
benpaquier <hello@benoitpaquier.com>
2022-09-07 10:02:44 +0200

    Setup dependencies

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json('e-commerce api')
-})
+app.use(express.json())
+app.use(cors(process.env.FRONTEND_URL))
+app.use(morgan('tiny'))
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on ${process.env.PORT}`)
