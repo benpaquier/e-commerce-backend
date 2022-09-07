95b637bf239a3f19699e54809c2da2b9be32ac04
benpaquier <hello@benoitpaquier.com>
2022-09-07 10:10:25 +0200

    Fix morgan usage

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ require('./models')
 
 app.use(express.json())
 app.use(cors(process.env.FRONTEND_URL))
-app.use(morgan('tiny'))
+
+if (process.env.ENV === 'local') {
+  app.use(morgan('tiny'))
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on ${process.env.PORT}`)
